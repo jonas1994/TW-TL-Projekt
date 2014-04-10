d0e87ec046aad51d37607268c197c2603323f063
jonas1994 <nope825@googlemail.com>
2014-04-10 14:18:38 +0200

    DIes und das

---
+++ register.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title> Registrieren </title>
+</head>
+<body>
+<b> Server: </b>
+<input type="text" name="server" size="10"><br>
+<b> User: </b>
+<input type="text" name="user" size="10"><br>
+<b> Passwort: </b>
+<input type="text" name="password" size="10"><br>
+<b> Name: </b>
+<input type="text" name="name" size="10"><br>
+
+<?php
+
+	
+?>
+</body>
+</html>
