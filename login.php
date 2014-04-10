af986f67538213bbd39b8ee00251596aeffd7dff
jonas1994 <nope825@googlemail.com>
2014-04-10 15:59:57 +0200

    änderung login

--- login.php
+++ login.php
@@ -20,13 +20,49 @@ if(!$verbindung)
 if(!mysql_select_db($db_name,$verbindung))
 	die("Die Datenbank kann nicht angesprochen werden.");
 mysql_close($verbindung);
+$query = "SELECT Benutzername,Passwort FROM Anmeldedaten";
+		$ergebnis = mysql_query($query,$verbindung);
 
-while($daten = mysql_query($verbindung)
-if($ergebnis)
+while($daten = mysql_query($ergebnis)
 {
-	echo mysql_error();
+			if($id == $daten[0]) // Übeprüft ob es den Login Namen gibt
+			{
+				if($pw == $daten[1]) //Überprüft ob Login Name und Passwort zusammenpassen
+				{
+					$_SESSION['id'] = $id; //Session Variable setzen
+					$_SESSION['status'] = 'Logout'; //Setzt den Value des Buttons auf Logout
+						
+					//Name und Vorname der eingeloggten Person aus der Datenbank auslesen und in Session Variablen speichern
+					$query = "SELECT Nachname,Vorname FROM person WHERE p_id = " .$daten[2];
+					$ergebnis = mysql_fetch_array(mysql_query($query,$verbindung));
+					$_SESSION['Vorname'] = $ergebnis[1];
+					$_SESSION['Nachname'] = $ergebnis[0];
+					$_SESSION['loginerr'] = '';
+					break;
+				}
+				else
+				{
+					$_SESSION['loginerr'] = 'Falsches Passwort eingegeben...'; //Error
+					break;	
+				}
+			}
+			else
+			{
+				$_SESSION['loginerr'] = 'Falsche Logindaten eingegeben...'; //Error
+			}
+			$daten++;
+		}
+		
+		if(isset($_SESSION['id'])) // Wenn erfolgreich eingeloggt
+		{
+			header('Location: hauptseite.html');	//Weiterleitung zur Hauptseite
+		}
+		else //Sonst zurück zur Loginseite
+		{
+			header('Location: index.html');	
+		}
 }
-	
-?>
+		
+	?>
 </body>
 </html>
--- register.php
+++ register.php
@@ -37,6 +37,7 @@ Copyright © JoTi </div>	<!-- Text -->
 </div>
 <?php
 
+
 ?>
 
  
